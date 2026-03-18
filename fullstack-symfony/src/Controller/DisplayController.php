274f3799f327f69deb2fff9a7dee48beb5cdbda5
natan5042 <Natan.ARNAUD@etu.uca.fr>
2026-03-18 15:46:30 +0100

    vue mais bug

---
+++ fullstack-symfony/src/Controller/DisplayController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Character;
+use App\Repository\CharacterClassRepository;
+use App\Repository\CharacterRepository;
+use App\Repository\PartyRepository;
+use App\Repository\RaceRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class DisplayController extends AbstractController
+{
+    public function __construct(
+        private readonly CharacterRepository $characterRepository,
+        private readonly PartyRepository $partyRepository,
+        private readonly RaceRepository $raceRepository,
+        private readonly CharacterClassRepository $characterClassRepository,
+    )
+    {
+    }
+
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return $this->render('pages/home.html.twig', [
+            'characterCount' => $this->characterRepository->count([]),
+            'partyCount' => $this->partyRepository->count([]),
+            'raceCount' => $this->raceRepository->count([]),
+            'classCount' => $this->characterClassRepository->count([]),
+        ]);
+    }
+
+    #[Route('/characters', name: 'app_character_index', methods: ['GET'])]
+    public function characters(Request $request): Response
+    {
+        $search = trim((string) $request->query->get('q', ''));
+
+        $queryBuilder = $this->characterRepository->createQueryBuilder('c')
+            ->orderBy('c.name', 'ASC');
+
+        if ($search !== '') {
+            $queryBuilder
+                ->andWhere('LOWER(c.name) LIKE :search')
+                ->setParameter('search', '%'.mb_strtolower($search).'%');
+        }
+
+        return $this->render('pages/character/index.html.twig', [
+            'characters' => $queryBuilder->getQuery()->getResult(),
+            'filters' => [
+                'q' => $search,
+            ],
+        ]);
+    }
+
+    #[Route('/characters/{id}', name: 'app_character_show', requirements: ['id' => '\\d+'], methods: ['GET'])]
+    public function characterShow(int $id): Response
+    {
+        $character = $this->characterRepository->find($id);
+        if (!$character) {
+            throw $this->createNotFoundException('Character not found.');
+        }
+        \assert($character instanceof Character);
+
+        return $this->render('pages/character/show.html.twig', [
+            'character' => $character,
+            'abilities' => [
+                'STR' => $character->getStrength(),
+                'DEX' => $character->getDexterity(),
+                'CON' => $character->getConstitution(),
+                'INT' => $character->getIntelligence(),
+                'WIS' => $character->getWisdom(),
+                'CHA' => $character->getCharisma(),
+            ],
+        ]);
+    }
+
+    #[Route('/parties', name: 'app_party_index', methods: ['GET'])]
+    public function parties(Request $request): Response
+    {
+        return $this->render('pages/party/index.html.twig', [
+            'parties' => $this->partyRepository->findBy([], ['name' => 'ASC']),
+        ]);
+    }
+
+    #[Route('/parties/{id}', name: 'app_party_show', requirements: ['id' => '\\d+'], methods: ['GET'])]
+    public function partyShow(int $id): Response
+    {
+        $party = $this->partyRepository->find($id);
+        if (!$party) {
+            throw $this->createNotFoundException('Party not found.');
+        }
+
+        return $this->render('pages/party/show.html.twig', [
+            'party' => $party,
+        ]);
+    }
+}
